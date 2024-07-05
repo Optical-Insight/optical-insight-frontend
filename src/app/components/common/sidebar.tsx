c0f3ba90be0594eac2dc305370fd5e45e33ffa37
Kithmina Siriwardana <it21020230@my.sliit.lk>
2024-07-05 12:16:31 +0530

    trying github actions

--- src/app/components/common/sidebar.tsx
+++ src/app/components/common/sidebar.tsx
@@ -97,21 +97,7 @@ const AppSidebar = ({ tab }: { tab: string }) => {
         <Image
           className={`m-auto ${isShrunk ? "hidden" : "block"}`}
           alt={"Optical Insight Logo"}
-          src="/public/assets/images/logo.png"
-          width={172}
-          height={68}
-        />
-        <Image
-          className={`m-auto ${isShrunk ? "hidden" : "block"}`}
-          alt={"Optical Insight Logo"}
-          src="./public/assets/images/logo.png"
-          width={172}
-          height={68}
-        />
-        <Image
-          className={`m-auto ${isShrunk ? "hidden" : "block"}`}
-          alt={"Optical Insight Logo"}
-          src="/assets/images/logo.png"
+          src="/logo.png"
           width={172}
           height={68}
         />
