98c6f0bc03769deb4711f904e8838462703f54be
IT21018596 <it21018596@my.sliit.lk>
2024-07-21 13:25:00 +0530

    all tables

--- src/app/dashboard/[tab]/components/lab-technicians/lab-technicians-list.tsx
+++ src/app/dashboard/[tab]/components/lab-technicians/lab-technicians-list.tsx
@@ -1,9 +1,41 @@
 import React from "react";
 import { InstituteListAllProps } from "@/utils/interfaces";
-import CommonBtn from "@/app/components/common/button";
+
 import SearchFilter from "@/app/components/common/search-filter";
+import rows from "./table-data";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TablePaginationActions from "./table-pagination";
+import { TableHead } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+import CommonRegisterBtn from "@/app/components/common/registerButton";
 
 const InstituteListAll = ({ setActiveHeading }: InstituteListAllProps) => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: any) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   return (
     <div>
       <div className="flex justify-between mb-[25px] items-center ">
@@ -11,7 +43,7 @@ const InstituteListAll = ({ setActiveHeading }: InstituteListAllProps) => {
           List of all Lab Technicians
         </div>
         <div className="flex h-[42px]">
-          <CommonBtn
+          <CommonRegisterBtn
             label="Register new Lab Technician"
             onClick={() => setActiveHeading && setActiveHeading(2)}
           />
@@ -23,7 +55,7 @@ const InstituteListAll = ({ setActiveHeading }: InstituteListAllProps) => {
         labelSearch="Search for a Lab Technician"
         labelSelectOne="Status"
         labelSelectTwo="Location"
-        placeholderSearch="Search by Name or Institute Name"
+        placeholderSearch="Search by Name"
         optionsSelectOne={[
           { value: "active", label: "Active" },
           { value: "inactive", label: "Inactive" },
@@ -36,6 +68,77 @@ const InstituteListAll = ({ setActiveHeading }: InstituteListAllProps) => {
         ]}
         onSearch={() => {}}
       />
+
+      {/* Table - MUI */}
+      <div className="mb-[45px]">
+        <TableContainer component={Paper} className="rounded-lg">
+          <Table aria-label="custom pagination table">
+            <TableHead>
+              <TableRow className="bg-lightBlueBg font-bold h-[4.016vh]">
+                <TableCell className="font-bold">Name</TableCell>
+                <TableCell className="font-bold">Status</TableCell>
+                <TableCell className="font-bold">Location</TableCell>
+                <TableCell className="font-bold">Email</TableCell>
+                <TableCell className="font-bold">Registered Date</TableCell>
+                <TableCell className="font-bold">Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? rows.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : rows
+              ).map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell>{row.status}</TableCell>
+                  <TableCell>{row.location}</TableCell>
+                  <TableCell>{row.email}</TableCell>
+                  <TableCell>{row.date}</TableCell>
+                  <TableCell>
+                    <div>
+                      <MoreVertIcon />
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter className="bg-lightBlueBg">
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                  colSpan={5}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  slotProps={{
+                    select: {
+                      inputProps: {
+                        "aria-label": "rows per page",
+                      },
+                      native: true,
+                    },
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </div>
+
+
     </div>
   );
 };
