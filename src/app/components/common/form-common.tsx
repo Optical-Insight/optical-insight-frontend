c4c90e5909352a38fd23624ef900965488f0b9ec
kithmina-siriwardana <it21020230@my.sliit.lk>
2024-09-10 09:22:20 +0530

    done

--- src/app/components/common/form-common.tsx
+++ src/app/components/common/form-common.tsx
@@ -9,19 +9,17 @@ const FormField = ({
   onChange,
   readOnly,
   type,
-  hasError, 
-  
+  hasError,
 }: FormFieldProps) => {
   return (
     <div className="flex items-center justify-between w-full mb-2">
-      
-        <label
-          htmlFor={label}
-          className="block text-[16px] text-darkText font-semibold"
-        >
-          {label} {required && <span className="text-red-500">*</span>}
-        </label>
-      
+      <label
+        htmlFor={label}
+        className="block text-[16px] text-darkText font-semibold"
+      >
+        {label} {required && <span className="text-red-500">*</span>}
+      </label>
+
       <input
         disabled={readOnly}
         required={required}
@@ -33,7 +31,8 @@ const FormField = ({
           ${hasError ? "border border-red-500" : "border border-gray-300"}
         `}
         placeholder={placeholder}
-        value={value}
+        // value={value}
+        defaultValue={value}
         onChange={(e) => onChange(e.target.value)}
       />
       {/*{hasError && <span className="text-red-500 text-xs mt-1">This field is required</span>}*/}
