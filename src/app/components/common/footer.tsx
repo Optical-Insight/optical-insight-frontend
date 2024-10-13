87621b05197f7afd464c9e3f1d520faa1990082b
Shehan Gunasekara <87413135+Shehan-Gunasekara@users.noreply.github.com>
2024-10-14 00:42:34 +0530

    footer added

---
+++ src/app/components/common/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-lightBg text-gray-700 py-4 text-center  ">
+      <p className="text-sm">
+        &copy; {new Date().getFullYear()} Optical Insight. All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
